00f26dd8e6d6a30cf77866562498907bb435ff9f
Jason Watson <jbw@jbw.cc>
2010-07-28 19:35:50 +0100

    Saner implementation.

--- lib/somefixtures/fixture.rb
+++ lib/somefixtures/fixture.rb
@@ -3,64 +3,39 @@ require 'HTTParty'
 module SomeFixtures
   class Fixture
     include HTTParty
-    
     format @format
-    
-    attr_accessor :locations  
-    def initialize details = { }
-   
-      @location = details[:location]
-      @format   = details[:format]
-      @base_uri = details[:base_uri]
-      @locations  = [] 
-      @name = []
-      
+        
+    def initialize fixture_info
+      @format   = fixture_info[:format]
+      @base_uri = fixture_info[:base_uri]
+      @save_to  = fixture_info[:save_to]
+      @fixtures = {}
     end
-    
-    # Sometimes you will need to be authenticated to :get the fixtures you need. 
-    def auth auth_params = {}
-      
-      @login = auth[:login]
-      @token = auth[:token]
-      
-    end
-    
+
     def add query, name
-      @name << name
-      fixture = @base_uri + query
-      @locations << fixture
-      
+      @fixtures.each_key { |key| print name == key ? "You have added \"#{key}\" for a file name already. Each filename must be unique!\n" : nil }
+      @fixtures[name] = @base_uri + query
     end
-    
-    def make_fixtures! 
-      
-      fixtures = get @locations
-      save fixtures
-      
+
+    def make_fixtures!
+      save (get @fixtures)
     end
-    
+
     private
-    
     def get fixtures
-      
-      response = []
-      fixtures.each do |f|
-        response << self.class.get(f)
-      end
-      
-      response
-      
+      responses = []
+      @fixtures.each_value{ |key| responses << self.class.get(key) }
+      responses
     end
-    
+
     def save fixtures
       i = 0
+      name = @fixtures.keys
       fixtures.each do |f|
-        new_fixture = File.new(@location + @name[i] + "." + @format, "w")
-        new_fixture.puts f
-        i += 1
-      end
-      
-    end 
+        File.new(name[i] + "." + @format, "w").puts f
+        i = i.succ
+      end  
+    end
     
   end
 end
