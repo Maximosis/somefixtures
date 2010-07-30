37c8b8a33b9a9d1b3bc52c8bfadb34ddc4a3ac1b
Jason Watson <jbw@jbw.cc>
2010-07-30 15:49:21 +0100

    authentication updated to include choice of which fixture is authenticated.

--- lib/somefixtures/fixture.rb
+++ lib/somefixtures/fixture.rb
@@ -1,26 +1,24 @@
+require 'cgi'
 require 'HTTParty'
 
 module SomeFixtures
   class Fixture
     include HTTParty
     format @format
-        
+    
     def initialize fixture_info
       @format   = fixture_info[:format]
       @base_uri = fixture_info[:base_uri]
       @save_to  = fixture_info[:save_to]
+      @login    = fixture_info[:login]
+      @token    = fixture_info[:token]
+      
       @fixtures = {}
     end
     
-    def login auth
-      @login = auth[:login]
-      @token = auth[:token]
-      @authenticated = true
-    end
-    
-    def add query, name
-      @fixtures.each_key { |key| print name == key ? "You have added \"#{key}\" for a file name already. Each filename must be unique!\n" : nil }
-      @fixtures[name] = @base_uri + query
+    def add query, name, authenticate = false
+      @fixtures.each_key { |key| print name == key ? "You have added \"#{key}\" for a file name already. Each filename must be unique!\n" : nil  }
+      @fixtures[name] = { :query => query, :authenticate => authenticate }
     end
 
     def make_fixtures!
@@ -30,16 +28,22 @@ module SomeFixtures
     private
     def get fixtures
       responses = []
-      if @authenticated
-        @fixtures.each_value{ |key| responses << self.class.get(key, :query => auth_params) }
-      else
-        @fixtures.each_value{ |key| responses << self.class.get(key) }
+
+      @fixtures.each_key do |name|
+        responses << self.class.get( @base_uri + @fixtures[name][:query], :query => (authenticated? name) )
       end
-        responses
+
+      responses
+    end
+
+    def authenticated? name
+      @authenticate = @fixtures[name][:authenticate]; 
+      auth_params
     end
 
     def save fixtures
       name = @fixtures.keys
+
       fixtures.each do |f|
         File.new(@save_to + name.first + "." + @format, "w").puts f
         name.shift
@@ -47,7 +51,7 @@ module SomeFixtures
     end
 
     def auth_params 
-      @login.nil? ? {} : { :login => @login, :token => @token }
+      if @authenticate; (@login.nil? ? {} : { :login => @login, :token => @token }); end
     end
 
   end
