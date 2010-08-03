c84e9cc124bae3b99281a6e7c716fea27b3d1ea1
Jason Watson <jbw@jbw.cc>
2010-08-04 00:08:46 +0100

    added spec for updating a repo's name.

--- spec/somefixtures/fixture.rb
+++ spec/somefixtures/fixture.rb
@@ -11,6 +11,14 @@ module SomeFixtures
           @valid.make_fixtures!['joecolly'].should == "joecolly"
           
         end
+        context "with parameters" do
+          it "should return sucessfully" do
+            stub_post("/repos/show/jbw/emacs", "repo_update.json")
+            @valid.add_post({ :route => "/repos/show/jbw/emacs", :values => "values[name]=emacs"},
+                            "repo_update_name", true)
+            @valid.make_fixtures!['emacs'].should == "emacs"
+          end
+        end
       end
     end
   end
